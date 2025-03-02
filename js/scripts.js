4d30c8f4596d6c1351e7d62df2f644b90547226b
Eric Junior <eric.faria2003@gmail.com>
2025-03-02 03:30:31 -0300

    ADicionado botao de chat e alterado banner

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+  // Exibe/oculta o chat ao clicar no botão de ajuda
+  document.getElementById('help-button').addEventListener('click', function() {
+    var chat = document.getElementById('chat-container');
+    chat.classList.toggle('d-none');
+  });
+
+  // Fecha o chat ao clicar no botão de fechar
+  document.getElementById('close-chat').addEventListener('click', function() {
+    document.getElementById('chat-container').classList.add('d-none');
+  });
